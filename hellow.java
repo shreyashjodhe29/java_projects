5cbb076cd88466dfea969ec6b93e8969448e9b59
shreyashjodhe29 <shreyashjodhe29@gmail.com>
2025-09-07 13:55:26 +0530

    Inital commit

---
+++ hellow.java
@@ -0,0 +1,11 @@
+package mypack;
+
+public class hellow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("World");
+	}
+
+}
